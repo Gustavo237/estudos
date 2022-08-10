a089c1547875a5c85de2de11700095e5d07294c7
Luis Gustavo <gustavo237@gmail.com>
2022-08-10 20:32:31 -0300

    segundo arquivo

--- exemplo 1 .py
+++
@@ -1,8 +0,0 @@
-# exercicios python
-# escreva 3  numeros a,b,c e de sua soma
-a = int(input('digite um numero '))
-b = int(input('digite outro numero '))
-c = int(input('digite mais um numero '))
-s = a + b + c
-print('a soma desses numeros vale {}' .format(s))
-
