f6c7aae9a40ab696ff3df31d3d0e0aadb31dfbad
Luis Gustavo <gustavo237@gmail.com>
2022-08-10 20:32:31 -0300

    segundo arquivo

---
+++ exemplo1.py
@@ -0,0 +1,2 @@
+# exercicios python
+# escreva 3  numeros a,b,c e de sua soma
---
+++ exemplo2.py
@@ -0,0 +1,9 @@
+# imprimir numeros de ponto floutuantes usando %n1.n2f
+ #o primeiro n1 e a quantidade minima de numeros antes da virgula e sera acrescido por espacos antes
+print('esse numero é real: %.2f' %(13.144))
+# o retorno sera 13.14
+print('esse numero é real: %.3f' %(13.144))
+# o retorno sera 13.144
+print('esse numero é real: %.4f' %(13.144))
+# o retorno sera 13.1440( ele adiciona um zero se nao houver outro numero
+
